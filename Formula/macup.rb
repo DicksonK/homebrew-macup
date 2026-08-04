5704bf998d761c537de0e0060f11ba045ebde3ca
Dickson K <dickson809@gmail.com>
2026-08-04 23:59:47 +1000

    Add macup formula v0.1.0

---
+++ Formula/macup.rb
@@ -0,0 +1,20 @@
+class Macup < Formula
+  desc "Bootstrap and keep a Mac dev environment in sync"
+  homepage "https://github.com/dicksonk/macup"
+  url "https://github.com/dicksonk/macup/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "a69ae06f6eb95cc6fa549421fcb22c9a05b35b7ff4f4cbba1116aa577a082c21"
+  license "MIT"
+
+  depends_on "git"
+  depends_on "gum"
+  depends_on "gh"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/macup"
+  end
+
+  test do
+    system "#{bin}/macup", "--help"
+  end
+end
